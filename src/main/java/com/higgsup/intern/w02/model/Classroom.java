4ea5c2e63628d5e4206c92d5285eaf1c9de4c148
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-28 23:07:55 +0700

    Create Classroom

---
+++ src/main/java/com/higgsup/intern/w02/model/Classroom.java
@@ -0,0 +1,40 @@
+package com.higgsup.intern.w02.model;
+
+public class Classroom {
+    private int id;
+    private String name;
+    private String description;
+    private int constructorId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getConstructorId() {
+        return constructorId;
+    }
+
+    public void setConstructorId(int constructorId) {
+        this.constructorId = constructorId;
+    }
+}
