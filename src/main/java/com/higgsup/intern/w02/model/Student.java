92a008dc9f0f48ac3c27f2e01e417245b6f3410e
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-28 23:08:29 +0700

    Create Student

---
+++ src/main/java/com/higgsup/intern/w02/model/Student.java
@@ -0,0 +1,40 @@
+package com.higgsup.intern.w02.model;
+
+public class Student {
+    private int id;
+    private String name;
+    private int yearOfBirth;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
