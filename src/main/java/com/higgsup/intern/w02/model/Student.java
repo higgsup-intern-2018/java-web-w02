be7616a6b645ec3af1a4fe43ebd3c56f8de7cf55
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-28 23:26:22 +0700

    Edit Student: change date of birth type: Number to int

--- src/main/java/com/higgsup/intern/w02/model/Student.java
+++ src/main/java/com/higgsup/intern/w02/model/Student.java
@@ -3,7 +3,7 @@ package com.higgsup.intern.w02.model;
 public class Student {
     private int id;
     private String name;
-    private Number yearOfBirth;
+    private int yearOfBirth;
     private String address;
 
     public int getId() {
@@ -22,11 +22,11 @@ public class Student {
         this.name = name;
     }
 
-    public Number getYearOfBirth() {
+    public int getYearOfBirth() {
         return yearOfBirth;
     }
 
-    public void setYearOfBirth(Number yearOfBirth) {
+    public void setYearOfBirth(int yearOfBirth) {
         this.yearOfBirth = yearOfBirth;
     }
 
