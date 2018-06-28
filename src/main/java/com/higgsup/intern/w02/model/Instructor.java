bc448cd1b18836c713964b4c287d303a762b6ac1
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-28 23:08:15 +0700

    Create Instructor

---
+++ src/main/java/com/higgsup/intern/w02/model/Instructor.java
@@ -0,0 +1,40 @@
+package com.higgsup.intern.w02.model;
+
+public class Instructor {
+    private int id;
+    private String name;
+    private int dateOfBirth;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(int dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
