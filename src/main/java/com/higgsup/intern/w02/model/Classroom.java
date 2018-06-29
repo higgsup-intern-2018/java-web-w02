d4a475d8e3a89c52362c91031646b42535e3cc9b
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-29 08:49:47 +0700

    Classroom class

---
+++ src/main/java/com/higgsup/intern/w02/model/Classroom.java
@@ -0,0 +1,49 @@
+package com.higgsup.intern.w02.model;
+
+public class Classroom
+{
+    private int id;
+    private String name;
+    private String description;
+    private int instructorId;
+
+    public Classroom(int id, String name, String description, int instructorId)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.instructorId = instructorId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getInstructorId() {
+        return instructorId;
+    }
+
+    public void setInstructorId(int instructorId) {
+        this.instructorId = instructorId;
+    }
+}
