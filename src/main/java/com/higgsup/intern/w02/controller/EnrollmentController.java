3b86e0454ecdbb82fe2650008302401a5d190e77
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-01 21:06:46 +0700

    Edit EnrollmentController

--- src/main/java/com/higgsup/intern/w02/controller/EnrollmentController.java
+++ src/main/java/com/higgsup/intern/w02/controller/EnrollmentController.java
@@ -1,5 +1,6 @@
 package com.higgsup.intern.w02.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.higgsup.intern.w02.dao.EnrollmentDAO;
 import com.higgsup.intern.w02.model.Enrollment;
 
@@ -8,9 +9,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
 
 public class EnrollmentController extends HttpServlet {
     private EnrollmentDAO dao;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println(objectMapper.writeValueAsString(dao.displayAll()));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int studentId = Integer.parseInt(request.getParameter("studentId"));
         int classroomId = Integer.parseInt(request.getParameter("classroomId"));
