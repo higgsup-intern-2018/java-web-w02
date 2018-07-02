f301c4521185765e44551ec7a5edca4c49a4c388
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-02 15:10:22 +0700

    Create StudentInfo

---
+++ src/main/java/com/higgsup/intern/w02/model/StudentInfo.java
@@ -0,0 +1,13 @@
+package com.higgsup.intern.w02.model;
+
+public class StudentInfo extends Student {
+    private String className;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
