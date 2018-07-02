7b97715004a58c527a975156516245210579f2e9
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-02 15:13:25 +0700

    Create StudentInfoController

---
+++ src/main/java/com/higgsup/intern/w02/controller/StudentInfoController.java
@@ -0,0 +1,36 @@
+package com.higgsup.intern.w02.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.higgsup.intern.w02.dao.EnrollmentDAO;
+import com.higgsup.intern.w02.dao.StudentDAO;
+import com.higgsup.intern.w02.dao.StudentInfoDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+public class StudentInfoController extends HttpServlet {
+    ObjectMapper objectMapper = new ObjectMapper();
+    private StudentInfoDAO dao;
+
+
+    public StudentInfoController() {
+        super();
+        dao = new StudentInfoDAO();
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println(objectMapper.writeValueAsString(dao.displayStudentInfo()));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
