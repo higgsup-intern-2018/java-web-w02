3a372d4e066048a196c50da9ba5c76a6f3d83d58
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-03 08:39:00 +0700

    create ClassroomInfo

---
+++ src/main/java/com/higgsup/intern/w02/model/ClassroomInfo.java
@@ -0,0 +1,22 @@
+package com.higgsup.intern.w02.model;
+
+public class ClassroomInfo extends Classroom {
+    private String instructorName;
+    private int studentCount;
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public int getStudentCount() {
+        return studentCount;
+    }
+
+    public void setStudentCount(int studentCount) {
+        this.studentCount = studentCount;
+    }
+}
