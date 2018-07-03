7a1cb866e9d2fdd2c9d1a5899db384c8aab54454
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-03 09:38:59 +0700

    create Response

---
+++ src/main/java/com/higgsup/intern/w02/model/Response.java
@@ -0,0 +1,13 @@
+package com.higgsup.intern.w02.model;
+
+public class Response {
+    private boolean success;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
