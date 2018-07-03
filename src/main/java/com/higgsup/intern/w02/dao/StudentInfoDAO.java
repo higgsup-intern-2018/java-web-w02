f94597378f4798e4f535a11dd6f0b17c3a3e379a
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-03 08:38:15 +0700

    edit StudentInfoDAO

--- src/main/java/com/higgsup/intern/w02/dao/StudentInfoDAO.java
+++ src/main/java/com/higgsup/intern/w02/dao/StudentInfoDAO.java
@@ -1,6 +1,6 @@
 package com.higgsup.intern.w02.dao;
 
-import com.higgsup.intern.w02.model.Student;
+import com.higgsup.intern.w02.model.Classroom;
 import com.higgsup.intern.w02.model.StudentInfo;
 import com.higgsup.intern.w02.util.DBUtil;
 
@@ -13,28 +13,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StudentInfoDAO implements Serializable {
-    public List<StudentInfo> displayStudentInfo() throws SQLException {
-
-        List<StudentInfo> studentInfoList = new ArrayList<>();
-        String sql = "SELECT student.id, student.name, student.year_of_birth, student.address, classroom.name FROM (enrollment " +
+    public StudentInfo displayStudentInfo(int id) throws SQLException {
+        StudentInfo studentInfo = new StudentInfo();
+        String sql = "SELECT student.*, enrollment.*, classroom.* FROM (enrollment " +
                 "INNER JOIN student ON student.id = enrollment.student_id)" +
-                "INNER JOIN classroom ON enrollment.classroom_id = classroom.id ";
+                "INNER JOIN classroom ON enrollment.classroom_id = classroom.id " +
+                "WHERE student.id =" + id;
         try (
                 Connection conn = DBUtil.getConnection();
                 Statement stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery(sql);
         ) {
-            System.out.println("Display information of students");
+            System.out.println("Display information of the student");
+
+            List<Classroom> classroomList = new ArrayList<>();
+
             while (rs.next()) {
-                StudentInfo studentInfo = new StudentInfo();
                 studentInfo.setId(rs.getInt("id"));
                 studentInfo.setName(rs.getString("name"));
                 studentInfo.setYearOfBirth(rs.getInt("year_of_birth"));
                 studentInfo.setAddress(rs.getString("address"));
-                studentInfo.setClassName(rs.getString("classroom.name"));
-                studentInfoList.add(studentInfo);
+
+                int classroomId = rs.getInt("enrollment.classroom_id");
+                String name = rs.getString("classroom.name");
+                String ClassroomDescription = rs.getString("classroom.description");
+                int instructorId = rs.getInt("classroom.instructor_id");
+
+                Classroom classroom = new Classroom();
+                classroom.setId(classroomId);
+                classroom.setName(name);
+                classroom.setDescription(ClassroomDescription);
+                classroom.setInstructorId(instructorId);
+
+                classroomList.add(classroom);
             }
+
+            studentInfo.setClassroomList(classroomList);
         }
-        return studentInfoList;
+        return studentInfo;
     }
 }
